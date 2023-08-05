b8e2f1aa1238793b93f721aa2ae751e5ec972eb4
nderituc <nderituc@mail.gvsu.edu>
2023-08-05 01:56:30 -0400

    Changes

--- App/app.R
+++ App/app.R
@@ -1,5 +1,10 @@
 library(shiny)
 library(ggplot2)
+library(dplyr)
+library(parsnip)
+library(skimr)
+library(GGally) # Load GGally for ggpairs
+library(broom)  # Load broom for tidying model output
 
 ui <- fluidPage(
   titlePanel("Multiple Linear Regression App"),
@@ -12,7 +17,10 @@ ui <- fluidPage(
     ),
     mainPanel(
       plotOutput("regression_plot"),
-      verbatimTextOutput("regression_summary")
+      tableOutput("regression_metrics_table"),
+      tableOutput("regression_coefs_table"),
+      plotOutput("ggpairs_plot"),
+      verbatimTextOutput("data_summary"),
     )
   )
 )
@@ -52,8 +60,13 @@ server <- function(input, output, session) {
     # Prepare the formula for multiple linear regression
     formula <- as.formula(paste(response_var, "~", paste(predictor_vars, collapse = "+")))
     
-    # Perform multiple linear regression using lm() function
-    model <- lm(formula, data = data)
+    # Perform multiple linear regression using parsnip package
+    lm_spec <- linear_reg() %>% 
+      set_mode("regression") %>%
+      set_engine("lm")
+    
+    mlr_mod <- lm_spec %>% 
+      fit(formula, data = data)
     
     # Generate the plot using ggplot2
     ggplot(data, aes_string(x = paste(predictor_vars, collapse = "+"), y = response_var)) +
@@ -64,8 +77,37 @@ server <- function(input, output, session) {
            y = paste("Response Variable:", response_var))
   })
   
-  # Show the summary of the linear regression model
-  output$regression_summary <- renderPrint({
+  # Create the dataset summary
+  output$data_summary <- renderPrint({
+    data <- data_reactive()
+    if (is.null(data))
+      return(NULL)
+    
+    cat("Dataset Summary:\n")
+    cat("Number of Rows:", nrow(data), "\n")
+    cat("Number of Columns:", ncol(data), "\n")
+    cat("Column Names:", paste(names(data), collapse = ", "), "\n")
+  })
+  
+  # Create the ggpairs plot
+  output$ggpairs_plot <- renderPlot({
+    data <- data_reactive()
+    response_var <- input$response_var
+    predictor_vars <- c(input$predictor_vars, response_var) # Include the response variable
+    
+    if (is.null(data) || length(predictor_vars) == 0)
+      return(NULL)
+    
+    # Filter data to include only selected variables
+    data_filtered <- data %>%
+      select(all_of(predictor_vars))
+    
+    # Create the ggpairs plot using GGally
+    ggpairs(data_filtered)
+  })
+  
+  # Create the model summary table for model metrics
+  output$regression_metrics_table <- renderTable({
     data <- data_reactive()
     response_var <- input$response_var
     predictor_vars <- input$predictor_vars
@@ -76,12 +118,73 @@ server <- function(input, output, session) {
     # Prepare the formula for multiple linear regression
     formula <- as.formula(paste(response_var, "~", paste(predictor_vars, collapse = "+")))
     
-    # Perform multiple linear regression using lm() function
-    model <- lm(formula, data = data)
+    # Perform multiple linear regression using parsnip package
+    lm_spec <- linear_reg() %>% 
+      set_mode("regression") %>%
+      set_engine("lm")
     
-    # Show the summary of the linear regression model
-    summary(model)
+    mlr_mod <- lm_spec %>% 
+      fit(formula, data = data)
+    
+    # Get the fitted model using extract_fit_engine() from parsnip
+    fitted_model <- extract_fit_engine(mlr_mod)
+    
+    # Get the model summary using broom::glance
+    model_glance <- broom::glance(fitted_model)
+    
+    # Create a data frame for the model metrics
+    model_metrics <- data.frame(
+      Metric = c("R-squared", "Adjusted R-squared", "AIC", "BIC", "F-statistic"),
+      Value = c(
+        model_glance$r.squared, 
+        model_glance$adj.r.squared, 
+        AIC(fitted_model),
+        BIC(fitted_model),
+        summary(fitted_model)$fstatistic[1]
+      )
+    )
+    
+    model_metrics
+  })
+  
+  # Create the model summary table for model coefficients
+  output$regression_coefs_table <- renderTable({
+    data <- data_reactive()
+    response_var <- input$response_var
+    predictor_vars <- input$predictor_vars
+    
+    if (is.null(data) || is.null(response_var) || length(predictor_vars) == 0)
+      return(NULL)
+    
+    # Prepare the formula for multiple linear regression
+    formula <- as.formula(paste(response_var, "~", paste(predictor_vars, collapse = "+")))
+    
+    # Perform multiple linear regression using parsnip package
+    lm_spec <- linear_reg() %>% 
+      set_mode("regression") %>%
+      set_engine("lm")
+    
+    mlr_mod <- lm_spec %>% 
+      fit(formula, data = data)
+    
+    # Get the fitted model using extract_fit_engine() from parsnip
+    fitted_model <- extract_fit_engine(mlr_mod)
+    
+    # Get the model coefficients using broom::tidy
+    model_coefs <- broom::tidy(fitted_model)
+    
+    model_coefs
   })
 }
 
 shinyApp(ui = ui, server = server)
+
+
+
+
+
+
+
+
+
+
